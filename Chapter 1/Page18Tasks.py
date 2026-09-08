4a9c21c04287a953ef18bbaa232ba32886610e89
BeatriceD09 <22.beatrice.dowling@msltcastlegregory.ie>
2026-09-08 16:18:16 +0100

    Add tasks for age, arithmetic, and distance calculations

---
+++ Chapter 1/Page18Tasks.py
@@ -0,0 +1,61 @@
+# 1
+age = 17
+print("1.")
+print(age,"What a great age!")
+print(" ")
+
+# 2
+x = 12.3
+y = 5.5
+sum = x+y
+difference = x-y
+product = x*y
+print("2.")
+print("Sum =",sum)
+print("Differance =",difference)
+print("Product =",product)
+print(" ")
+
+# 3
+x = 20.4
+y = 13.7
+average = (x+y)/2
+remainder = (x%y)
+power = x**y
+print("3.")
+print("Average =",average)
+print("Remainder =",remainder)
+print("1st to the power of the 2nd =",power)
+print(" ")
+
+# 5
+# distance from Singapore to school = approximately 7127 miles
+# 1 mile = 1.60935 kilometers
+# 7127 miles * 1.60935
+# 1 kilometer = 900.00
+answer_km = 11469.83745
+answer = round(answer_km,)
+distance_km = 1.60935*7127
+distance_m = 7127
+cost = (7127)*(1.60935)*(900)
+print("5.")
+print("Distance from Singapore to school =",distance_m,"miles")
+print("Distance from Singapore to school =",answer,"kilometers")
+print("Cost = €",cost)
+print(" ")
+
+# 6
+# assume Pi = 3.14
+# number of cylinders = totalLiquid // volume (ignore remainders)
+# totalLiquid // volume
+# number of cylinders = 3515 // volume
+totalLiquid = 3515
+volume = (3.14)*(5**2)*(20)
+number_cylinders = totalLiquid // volume
+h = 20
+r = 5**2
+height = 25
+radius = 10
+print("6.")
+print("Volume =",volume)
+print("Number of cylinders needed =",number_cylinders)
