0985d7843951f06449b38fb0889f1c55fcd84648
BeatriceD09 <22.beatrice.dowling@msltcastlegregory.ie>
2026-09-15 15:50:30 -0100

    Implement driving licence eligibility checker
    Added a driving licence eligibility checker that prompts for age and displays eligibility message.

--- Chapter 1/Python,Studyclix.py
+++ Chapter 1/Python,Studyclix.py
@@ -1,1 +1,22 @@
+#Question 16
+#(a)
+print("The program")
 
+age = int(input("What age are you?"))
+          
+if (age >= 17):
+          print("You are entitled to apply for a driving licence.")
+
+#(i) Program is welcoming the person to the driving licence eligibility checker
+print("Welcome to the driving licence elegibility checker")
+
+#(ii) Program is asking person for their age in an integer data type
+age = int(input("What age are you?"))
+
+#(iii) Program is stating the person age on the screen
+print("You entered",age,)
+
+if (age) >= 17:
+    print("You are entitled to apply for a driving licence.")
+    
+#(iv)
