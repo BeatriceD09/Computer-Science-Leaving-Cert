eefde2b3e20319168fca004eaa0e524da75e1750
BeatriceD09 <22.beatrice.dowling@msltcastlegregory.ie>
2026-09-17 09:39:14 +0000

    Add basic loops for bank balance and password check

---
+++ Chapter 1/Loops.py
@@ -0,0 +1,13 @@
+#1
+bankBalance = 0
+while bankBalance <= 0:
+    print("Sorry you have no money in your account")
+
+#2
+password = 12345
+myPassword = int(input("Enter your password "))
+while myPassword != password:
+    print("Sorry password is incorrect!")
+    break
+if myPassword == password:
+    print("Welcome!!!")
